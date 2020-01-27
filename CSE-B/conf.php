692c18046edeb5ab8fe82952c3246e168a1403ba
Rachna Singh <rachnas0410@gmail.com>
2020-01-27 11:42:44 +0530

    hiii

--- CSE-B/conf.php
+++ CSE-B/conf.php
@@ -6,10 +6,26 @@ $password ='';
 $databasename = 'cseb';
 $conn = mysqli_connect($hostname, $userName,$password,$databasename);
 if($conn){
-	echo" connection successful";
+	echo" connection successfull<br>";
 }
 else
 {
 	echo"no connection";
 }
+if(isset($_POST['btn']))
+{
+	$names = $_POST['name'];
+$ages = $_POST['age'];
+$cont = $_POST['contact'];
+$mail = $_POST['email'];
+
+	$qy = "insert into users(name , age,contact,email) values ('$names', '$ages', '$cont','$mail')";
+$qt=mysqli_query($conn, $qy);
+if($qt)
+{
+    echo "<script>alert('registeration Has been completed!')</script> ";
+    echo "<script>window.open('files.php','_self')</script>";
+}
+}
+
 ?>
